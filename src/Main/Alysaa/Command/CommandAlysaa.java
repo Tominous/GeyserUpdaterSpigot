ac98f1fb40df0511b3d28ddce82e5b6348362381
BuildTools <unconfigured@null.spigotmc.org>
2020-12-14 17:08:10 +0100

    Geyser and Floodgate Updater, not yet finished

---
+++ src/Main/Alysaa/Command/CommandAlysaa.java
@@ -0,0 +1,65 @@
+package Main.Alysaa.Command;
+
+import Alysaa.Tut.Alysaaproject;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class CommandAlysaa implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.hasPermission("AlysaaPlugin.geyserupdate");
+
+            if (command.getName().equalsIgnoreCase("geyserupdate")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&eDownloading the latest Geyser build. Please wait!"));
+                OutputStream os = null;
+                InputStream is = null;
+                String fileUrl = "https://ci.nukkitx.com/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar";
+                String outputPath = "File file = new File(\"..\" + System.getProperty(\"file.separator\") + plugin.getDataFolder().getPath())";
+                try {
+                    // create a url object
+                    URL url = new URL(fileUrl);
+                    // connection to the file
+                    URLConnection connection = url.openConnection();
+                    // get input stream to the file
+                    is = connection.getInputStream();
+                    // get output stream to download file
+                    os = new FileOutputStream(outputPath);
+                    final byte[] b = new byte[2048];
+                    int length;
+                    // read from input stream and write to output stream
+                    while ((length = is.read(b)) != -1) {
+                        os.write(b, 0, length);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    // close streams
+                    if (os != null) {
+                        try {
+                            os.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&aDownloading complete! You will need to restart/reload server before changes will take place."));
+            }
+        }
+        return true;
+    }
+}
---
+++ src/Main/Alysaa/Tut/Alysaaproject.java
@@ -0,0 +1,52 @@
+package Alysaa.Tut;
+
+import Main.Alysaa.Command.CommandAlysaa;
+import Main.Alysaa.Command.CommandKobe;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+import java.io.IOException;
+
+public class Alysaaproject extends JavaPlugin
+{
+    public static Alysaaproject plugin;
+
+    @Override
+    public void onEnable()
+    {
+
+        getLogger().info("Plugin has been enabled");
+        this.getCommand("geyserupdate").setExecutor(new CommandAlysaa());
+        this.getCommand("floodgateupdate").setExecutor(new CommandKobe());
+        createFiles();
+        plugin = this;
+    }
+
+    @Override
+    public void onDisable()
+    {
+        getLogger().info("Plugin has been disabled");
+
+    }
+
+    private File configf;
+    private FileConfiguration config;
+
+    private void createFiles () {
+        configf = new File(getDataFolder(), "config.yml");
+        if (!configf.exists()) {
+            configf.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+        config = new YamlConfiguration();
+
+        try {
+            config.load(configf);
+        } catch (IOException | InvalidConfigurationException e){
+            e.printStackTrace();
+        }
+
+    }
+}
